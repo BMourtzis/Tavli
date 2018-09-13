b49de63496c516d5332bf9164192354f36f25997
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2018-09-13 17:51:16 +0300

    added some items with materials and started with some scripts

---
+++ Tavli/Assets/Scripts/TavliBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavliBoard : MonoBehaviour {
+
+    TavliPlayer p1;
+    TavliPlayer p2;
+
+    TavliGameType gameType;
+
+    TavliPouli[] poulia;
+    TavliBoardPosition[] positions;
+    Die[] tavliDice;
+    
+    // Next Theme;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tavli/Assets/Scripts/TavliPouli.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavliPouli : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
